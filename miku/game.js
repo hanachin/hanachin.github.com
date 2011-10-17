15ff48fa6d91b23db097398bbef13f030a2fffae
hanachin <hanachin@gmail.com>
2011-10-18 07:28:44 +0900

    added onpu beam

--- miku/game.js
+++ miku/game.js
@@ -30,7 +30,7 @@ window.onload = function() {
 	
     game = new Game(320, 320);
     game.fps = 24;
-    game.preload('font.png', 'miku.png');
+    game.preload('font.png', 'miku.png', 'onpu.gif');
     game.onload = function() {
 		var miku = new Sprite(20, 28);
 		miku.x = (game.width - miku.width) / 2;
@@ -39,6 +39,32 @@ window.onload = function() {
 		miku.frame = 1;
 		miku.addEventListener('enterframe', function (e) {
 			miku.frame = Math.floor(game.frame / 2) % 3;
+		});
+		
+		game.rootScene.addEventListener('touchstart', function (e) {
+			var rad = radFromMiku(e);
+			var deg = degFromMiku(e);
+			
+			miku.rotation = deg;
+			
+			var onpu = new Sprite(18, 17);
+			onpu.x = miku.x + 1;
+			onpu.y = miku.y + 5;
+			onpu.image = game.assets['onpu.gif'];
+			onpu.frame = game.frame % 3;
+			onpu.rotation = deg;
+			onpu.addEventListener('enterframe', function (e) {
+				var speed = 10;
+				var vectorX = Math.cos(rad) * speed;
+				var vectorY = -Math.sin(rad) * speed;
+				onpu.x += vectorX;
+				onpu.y += vectorY;
+				if (onpu.x < 0 || game.width < onpu.x || onpu.y < 0 || game.height < onpu.y) {
+					game.rootScene.removeChild(onpu);
+				}
+			});
+			
+			game.rootScene.addChild(onpu);
 		});
 		
 		game.rootScene.addEventListener('touchmove', function (e) {
