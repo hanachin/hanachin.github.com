78206f3ff6d0798fd725fcf9ce65990832ba9a23
hanachin <hanachin@gmail.com>
2011-10-17 23:48:36 +0900

    miku-test

---
+++ miku/game.js
@@ -0,0 +1,24 @@
+Array.prototype.shuffle = function() {
+    var i = this.length;
+    while(i){
+        var j = Math.floor(Math.random()*i);
+        var t = this[--i];
+        this[i] = this[j];
+        this[j] = t;
+    }
+    return this;
+}
+
+enchant();
+
+count = 0;
+
+window.onload = function() {
+    var game = new Game(320, 320);
+    game.fps = 24;
+    game.preload('font.png', 'miku.gif');
+    game.onload = function() {
+    };
+    game.start();
+	window['game'] = enchant.Game.instance;
+};
