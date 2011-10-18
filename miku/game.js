3542eef2dca594fb558d24f5d6010109d0057557
hanachin <hanachin@gmail.com>
2011-10-19 01:19:47 +0900

    delete unused function

--- miku/game.js
+++ miku/game.js
@@ -1,14 +1,3 @@
-Array.prototype.shuffle = function() {
-    var i = this.length;
-    while(i){
-        var j = Math.floor(Math.random()*i);
-        var t = this[--i];
-        this[i] = this[j];
-        this[j] = t;
-    }
-    return this;
-}
-
 enchant();
 
 var otas = [];
